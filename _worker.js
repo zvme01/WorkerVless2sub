8b5243e0ddf80bd67d4c98bb3a91327161e6c159
zvme01 <153796243+zvme01@users.noreply.github.com>
2024-03-19 07:43:31 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -11,12 +11,11 @@ let addresses = [
 
 // 设置优选地址api接口
 let addressesapi = [
-	'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt' //可参考内容格式 自行搭建。
-];
+	'https://00ddd49b.zidonggengxin.pages.dev/ip.txt?token=shenger'
 
 let DLS = 4;//速度下限
 let addressescsv = [
-	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressescsv.csv' //iptest测速结果文件。
+	'https://00ddd49b.zidonggengxin.pages.dev/AS0-0-HKG.csv?token=shenger'
 ];
 
 let subconverter = "api.v1.mk"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
